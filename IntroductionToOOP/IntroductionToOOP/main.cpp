90e74abb3e9ec4dd8e2898d2de38884a80bda6ff
Alex <Alex@201-1>
2019-12-19 21:00:16 +0200

    Constructors.

--- IntroductionToOOP/IntroductionToOOP/main.cpp
+++ IntroductionToOOP/IntroductionToOOP/main.cpp
@@ -18,7 +18,7 @@ public:
 	}
 	void set_x(double x)
 	{
-		if (/*x >= -100 && x <= 100*/ abs(x)<=100)
+		if (/*x >= -100 && x <= 100*/ abs(x) <= 100)
 		{
 			this->x = x;
 		}
@@ -32,10 +32,48 @@ public:
 	{
 		this->y = y;
 	}
+
+	//			Constructors:
+	/*Point()
+	{
+		x = y = 0;
+		std::cout << "DefaultConstructor:\t" << this << std::endl;
+	}*/
+	Point(double x=0, double y=0)
+	{
+		this->x = x;
+		this->y = y;
+		std::cout << "Constructor:\t" << this << std::endl;
+	}
+	Point(const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		std::cout << "CopyConstructor:" << this << std::endl;
+	}
+	~Point()
+	{
+		std::cout << "Destructor:\t" << this << std::endl;
+	}
+
+	//		Operators:
+	void operator=(const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		std::cout << "CopyAssignment:" << this << std::endl;
+	}
+
+	//		Methods:
+	void print() const
+	{
+		std::cout << "x = " << x << "\t" << "y = " << y << std::endl;
+	}
 };
 
 //#define BASICS
-#define ENCAPSULATION
+//#define ENCAPSULATION
+//#define CONSTRUCTORS
 
 void main()
 {
@@ -71,4 +109,36 @@ void main()
 	std::cout << A.get_x() << "\t" << A.get_y() << std::endl;
 #endif // ENCAPSULATION
 
+#ifdef CONSTRUCTORS
+	const Point B;//Объявление константного объекта
+//std::cout << B.get_x() << std::endl;
+	B.print();
+	//Default constructor-
+
+	Point C = 5;
+	C.print();
+
+	Point D(6);
+	D.print();
+
+	Point E(5, 2);
+
+	//Copy constructor
+	Point F = E;
+	F.print();
+	Point G;
+	G = E;
+#endif // CONSTRUCTORS
+
+	Point A(5, 2);
+	A.print();
+
+	int a = 2;
+	int b = 3;
+	a = b;
+
+	Point B;	//Default constructor
+	B = A;		//Copy assignment
+	B.print();
+	//Copy assignment
 }
