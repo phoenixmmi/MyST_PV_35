757840500a5af9d12edb8b257fbdf7d3fa0b8ca2
Alex <Alex@202-9>
2020-02-06 21:14:11 +0200

    MoveMethods DONE; TODO: InitializerList;

--- IntroductionToOOP/String/main.cpp
+++ IntroductionToOOP/String/main.cpp
@@ -1,5 +1,8 @@
 #include<iostream>
 
+class String;
+String operator+(const String& left, const String& right);
+
 class String
 {
 	int size;	//Number of Bytes
@@ -13,7 +16,17 @@ public:
 	{
 		return str;
 	}
+	char* get_str()
+	{
+		return str;
+	}
 
+	/*String()
+	{
+		this->size = 80;
+		this->str = new char[size] {};
+		std::cout << "DefaultConstruct:" << this << std::endl;
+	}*/
 	String(int size = 80)
 	{
 		this->size = size;
@@ -36,12 +49,21 @@ public:
 		strcpy_s(this->str, size, other.str);
 		std::cout << "CopyConstructor:" << this << std::endl;
 	}
+	String(String&& other)
+	{
+		this->size = other.size;
+		this->str = other.str;
+		other.str = nullptr;
+		std::cout << "MoveConstructor:\t" << this << std::endl;
+	}
 
 	~String()
 	{
 		delete[] this->str;
 		std::cout << "Destructor:\t" << this << std::endl;
 	}
+
+	//			Operators:
 
 	String& operator=(const String& other)
 	{
@@ -55,6 +77,21 @@ public:
 		return *this;
 	}
 
+	String& operator+=(const String& other)
+	{
+		return *this = *this + other;
+	}
+
+	const char& operator[](int i)const
+	{
+		return str[i];
+	}
+
+	char& operator[](int i)
+	{
+		return str[i];
+	}
+
 	//			Methods:
 	void print()
 	{
@@ -62,13 +99,31 @@ public:
 	}
 };
 
+String operator+(const String& left, const String& right)
+{
+	String cat = left.get_size() + right.get_size() - 1;
+
+	/*for (int i = 0; i < left.get_size(); i++)cat.get_str()[i] = left.get_str()[i];
+	for (int i = 0; i < right.get_size(); i++)
+		cat.get_str()[i + left.get_size() - 1] = right.get_str()[i];*/
+
+	for (int i = 0; i < left.get_size(); i++)cat[i] = left[i];
+	for (int i = 0; i < right.get_size(); i++)
+		cat[i + left.get_size() - 1] = right[i];
+
+	return cat;
+}
+
 std::ostream& operator<<(std::ostream& os, const String& obj)
 {
 	return os << obj.get_str();
 }
 
+//#define CONSTRUCTORS_CHECK
+
 void main()
 {
+#ifdef CONSTRUCTORS_CHECK
 	String str1;
 	str1.print();
 
@@ -85,4 +140,14 @@ void main()
 	str1.print();
 
 	std::cout << str1 << std::endl;
+#endif // CONSTRUCTORS_CHECK
+
+	String str1 = "Hello";
+	String str2 = "World";
+	std::cout << "\n----------------------------------\n";
+	String str3 = str1 + str2;
+	std::cout << "\n----------------------------------\n";
+	std::cout << str3 << std::endl;
+	//str1 += str2;
+	//std::cout << str1 << std::endl;
 }
