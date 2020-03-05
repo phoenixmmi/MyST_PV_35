8a0a0349dae4e3c339bee324fa3537dcf078af99
Alex <Alex@304-7>
2020-03-05 21:16:22 +0200

    Base methods DONE. TODO: Special members and operators.

--- DataContainers/ForwardList/main.cpp
+++ DataContainers/ForwardList/main.cpp
@@ -1,28 +1,37 @@
 #include<iostream>
 
+#define delimiter "\n----------------------------------------\n"
+//static . -> (:: - Scope operator)
 class Element
 {
 	int Data;		//Значение элемента
 	Element* pNext;	//Адрес следующего элемента
+	static int count;
 public:
-	Element(int Data, Element* pNext = nullptr):Data(Data), pNext(pNext)
+	Element(int Data, Element* pNext = nullptr) :Data(Data), pNext(pNext)
 	{
+		count++;
 		std::cout << "EConstructor:\t" << this << std::endl;
 	}
 	~Element()
 	{
+		count--;
 		std::cout << "EDestructor:\t" << this << std::endl;
 	}
 	friend class ForwardList;
 };
 
+int Element::count = 0;
+
 class ForwardList
 {
 	Element* Head;
+	unsigned int Size;
 public:
 	ForwardList()
 	{
 		Head = nullptr;	//Если список пуст, то его голова указывает на 0;
+		Size = 0;
 		std::cout << "LConstructor:\t" << this << std::endl;
 	}
 	~ForwardList()
@@ -36,6 +45,7 @@ public:
 		Element* New = new Element(Data);
 		New->pNext = Head;
 		Head = New;
+		Size++;
 	}
 	void push_back(int Data)
 	{
@@ -50,6 +60,50 @@ public:
 			Temp = Temp->pNext;
 		}
 		Temp->pNext = new Element(Data);
+		Size++;
+	}
+
+	void insert(int Index, int Data)
+	{
+		if (Index > Size)
+		{
+			std::cout << "Error: Out of range" << std::endl;
+			return;
+		}
+		if (Index == 0)
+		{
+			push_front(Data);
+			return;
+		}
+		Element* Temp = Head;
+		for (int i = 0; i < Index - 1; i++)
+		{
+			Temp = Temp->pNext;
+		}
+		Element* New = new Element(Data);
+		New->pNext = Temp->pNext;
+		Temp->pNext = New;
+		Size++;
+	}
+
+	//			Removing elements:
+	void pop_front()
+	{
+		Element* buffer = Head;	//Запоминаем адрес удаляемого элемента
+		Head = Head->pNext;	//Исключаем элемент из списка
+		delete buffer;
+		Size--;
+	}
+	void pop_back()
+	{
+		Element* Temp = Head;
+		while (Temp->pNext->pNext != nullptr)
+		{
+			Temp = Temp->pNext;
+		}
+		delete Temp->pNext;
+		Temp->pNext = nullptr;
+		Size--;
 	}
 
 	//			Methods:
@@ -62,6 +116,7 @@ public:
 			std::cout << Temp << "\t" << Temp->Data << "\t" << Temp->pNext << std::endl;
 			Temp = Temp->pNext;	//Переход на следующий элемент
 		}
+		std::cout << "List size: " << Size << " elements.\n";
 	}
 };
 
@@ -79,4 +134,31 @@ void main()
 	}
 	fl.push_back(1024);
 	fl.print();
+	std::cout << delimiter << std::endl;
+	/*fl.pop_front();
+	fl.pop_back();
+	fl.print();*/
+
+	ForwardList fl2;
+	fl2.push_back(3);
+	fl2.push_back(5);
+	fl2.push_back(8);
+	fl2.push_back(13);
+	fl2.push_back(21);
+	fl2.print();
+
+	int index;
+	int value;
+	std::cout << "Input index: "; std::cin >> index;
+	std::cout << "Input value: "; std::cin >> value;
+	fl.insert(index, value);
+	fl.print();
+
+	
+
+	/*int* pa = new int(2);
+	delete[] pa;
+
+	int* arr = new int[5] {3, 5, 8, 13, 21};
+	delete arr;*/
 }
