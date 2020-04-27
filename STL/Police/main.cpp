ebad54ca31686cd49c54f57374f0f188dba65cb2
Михаил Маслак <maslak10055@gmail.com>
2020-04-27 18:51:21 +0300

    menu

--- STL/Police/main.cpp
+++ STL/Police/main.cpp
@@ -4,6 +4,7 @@
 #include<map>
 #include<list>
 #include<Windows.h>
+#include <conio.h>
 //#include"D:\Install\Dev\boost_1_72_0\boost\algorithm\string.hpp"
 #include<boost/algorithm/string.hpp>
 
@@ -13,20 +14,25 @@ void print_full_base(const std::map<std::string, std::list<std::string>>& base);
 void save(const std::map<std::string, std::list<std::string>>& base);
 void load(std::map<std::string, std::list<std::string>>& base);
 void insert(std::map<std::string, std::list<std::string>>& base);
+void find(std::map<std::string, std::list<std::string>>& base);
+int choice(int iterator);
+void menu();
 
 void main()
 {
 	setlocale(LC_ALL, "");
+	/*
 	//map - это контейнер, который хранит данные в виде Бинарного дерева. 
 	//Каждый элемент этого дерева представляет собой ПАРУ: key - value.
 	//std::cout << typeid(std::string("Hello").c_str()).name() << std::endl;
 	std::map<std::string, std::list<std::string>> base;// = init();
 	load(base);
-#define delimiter "\n------------------------------------\n"
 	print_full_base(base);
-	/*insert(base);
+	insert(base);
 	print_full_base(base);
 	save(base);*/
+#define delimiter "\n------------------------------------\n"
+	menu();
 }
 
 std::map<std::string, std::list<std::string>> init()
@@ -155,5 +161,82 @@ void insert(std::map<std::string, std::list<std::string>>& base)
 	{
 		//Создаем нового нарушителя в базе:
 		base.insert(std::pair<std::string, std::list<std::string>>(license_plate, { violation }));
+	}
+}
+void find(std::map<std::string, std::list<std::string>>& base)
+{
+	std::string license_plate;
+
+	std::cout << "Введите номер автомобиля: ";
+	SetConsoleCP(1251);	std::getline(std::cin, license_plate);	SetConsoleCP(866);
+
+	//std::map<std::string, std::list<std::string>>::iterator offender = base.find(license_plate);
+	std::cout << license_plate << " - ";
+	for (std::list<std::string>::iterator it = base.find(license_plate)->second.begin(); it != base.find(license_plate)->second.end(); it++)
+	{
+		std::cout << *it << ",";
+	}
+	std::cout << "\b;\n";
+}
+int choice(int iterator)
+{
+	int choice = _getch();
+	system("cls");
+	std::cout << choice << std::endl;
+	if (choice == 224)choice = _getch();
+	if (choice == 72)
+	{
+		if (iterator > 1)iterator--;
+		else iterator = 4;
+	}
+	else if (choice == 80/*choice==224*/)
+	{
+		if (iterator < 4)iterator++;
+		else iterator = 1;
+	}
+	else if (choice != 13) std::cout << "Неверный ввод" << std::endl;
+	else
+	{
+		std::cout << "Error";
+	}
+
+	std::cout << "Сделайте Ваш выбор: " << std::endl;
+	(iterator == 1) ? std::cout << "==>" : std::cout << "   ";
+	std::cout << "Загрузка базы из файла" << std::endl;
+	(iterator == 2) ? std::cout << "==>" : std::cout << "   ";
+	std::cout << "Сохранение базы в файл" << std::endl;
+	(iterator == 3) ? std::cout << "==>" : std::cout << "   ";
+	std::cout << "Вывод базы на экран" << std::endl;
+	(iterator == 4) ? std::cout << "==>" : std::cout << "   ";
+	std::cout << "Вывод правонарушений по указанному номеру" << std::endl;
+	std::cout << delimiter;
+	return{ iterator };
+};
+
+
+void menu()
+{
+	int iterator = 1;
+	std::map<std::string, std::list<std::string>> base = init();
+	do
+	{
+		iterator = choice(iterator);
+		//std::cout << choice;
+	} while (_getch() != 13);
+	if (iterator == 1)
+	{
+		load(base);
+	}
+	else if (iterator == 2)//сохранение базы в файл
+	{
+		save(base);
+	}
+	else if (iterator == 3)//вывод базы на екран
+	{
+		print_full_base(base);
+	}
+	else if (iterator == 4)//вывод правонарушений по указанному номеру
+	{
+		find(base);
 	}
 }
